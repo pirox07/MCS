ead16230b27dcd24de2b7c7e790e500508469bd8
pirox07 <39739315+pirox07@users.noreply.github.com>
2018-08-17 12:10:09 +0900

    fix 6

--- _lambda/build.sh
+++ _lambda/build.sh
@@ -6,10 +6,12 @@ cd `dirname $0`
 cat swagger-template.yaml | sed "s/account_placeholder/${AWS_ACCOUNT}/g" \
                           | sed "s/region_placeholder/${AWS_REGION}/g" \
                           > swagger.yaml
+cat swagger.yaml
 
 cat sam-base.yaml | sed "s/bucket_placeholder/${AWS_BUCKET}/g" \
                   | sed "s/role_arn_MCS-lambda/${MCS_LAMBDA_ROLE_ARN}/g" \
                   > sam-template.yaml
+cat sam-template.yaml
 
 aws cloudformation package --template-file sam-template.yaml --output-template-file ../sam-output.yaml --s3-bucket ${AWS_BUCKET} --s3-prefix lambda
 
