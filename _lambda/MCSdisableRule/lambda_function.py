57509139555f183b97ad530e4061a15948033ef3
Tetsuya Shinone <info@semnil.com>
2018-08-18 14:26:01 +0900

    Add the X-Ray SDK.

--- _lambda/MCSdisableRule/lambda_function.py
+++ _lambda/MCSdisableRule/lambda_function.py
@@ -1,5 +1,14 @@
 import boto3
+import os
 import json
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'vendored'))
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.core import patch
+
+patch(['boto3'])
+
 client = boto3.client('events')
 
 def lambda_handler(event, context):
--- _lambda/MCSgetSchedule/lambda_function.py
+++ _lambda/MCSgetSchedule/lambda_function.py
@@ -1,7 +1,16 @@
+import boto3
+import json
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'vendored'))
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.core import patch
+
+patch(['boto3'])
+
 def lambda_handler(event, context):
-    import boto3
-    import json
-    
+
     test_event = boto3.client('events', region_name='us-east-1')
     rule_Name = 'exec_time'
     
